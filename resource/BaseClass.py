d7bd7e4f3db8d3babcdbc6c5b15dc320cb5d4df3
Kral Markus <m.kral@plan-net.com>
2017-02-01 15:00:08 +0100

    session into base-class

--- resource/BaseClass.py
+++ resource/BaseClass.py
@@ -1,16 +1,29 @@
 from flask.views import MethodView
-
 from sqlalchemy import create_engine, engine
-class WaterBase(MethodView):
+from sqlalch import Base
+from sqlalch import Base, Data
+from sqlalchemy.orm import sessionmaker
 
+
+class WaterBase(MethodView):
     mysqlconnection = None;
+    mysqlsession = None;
 
     @property
     def mysqlconn(self):
         if self.mysqlconnection:
             return self.mysqlconnection
         else:
-            mysqlconnection = create_engine('mysql://hmpblv:ahs7ThasaiMioj@localhost/hmpblv')
-            return mysqlconnection
+            self.mysqlconnection = create_engine('mysql://hmpblv:ahs7ThasaiMioj@localhost/hmpblv')
+            Base.metadata.bind = self.mysqlconnection
+            return self.mysqlconnection
 
 
+    @property
+    def getsession(self):
+        if self.mysqlsession:
+            return self.mysqlsession
+        else:
+            session = sessionmaker(bind=self.mysqlconnection)
+            self.mysqlsession = session()
+            return self.mysqlsession
