8696afc850e4095647f035db36d6085032a7a029
Kevin Stieglitz <stieglit@hm.edu>
2017-02-03 11:16:02 +0100

    Rebuild Base

--- resource/BaseClass.py
+++ resource/BaseClass.py
@@ -7,3 +7,24 @@ from sqlalch import User as users
 
 class WaterBase(MethodView):
     mysqlconnection = None
+    mysqlsession = None
+
+    @property
+    def mysqlconn(self):
+        if self.mysqlconnection:
+            return self.mysqlconnection
+        else:
+            self.mysqlconnection = create_engine('mysql://root@localhost/hmpblv')
+            Base.metadata.bind = self.mysqlconnection
+            return self.mysqlconnection
+
+
+    @property
+    def getsession(self):
+        if self.mysqlsession:
+            return self.mysqlsession
+        else:
+            session = sessionmaker()
+            session.configure(bind=self.mysqlconn)
+            self.mysqlsession = session()
+            return self.mysqlsession
