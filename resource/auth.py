b87013d39ceb630ce169570416866e615f971ad4
Kevin Stieglitz <stieglit@hm.edu>
2017-02-07 09:48:29 +0100

    Update auth

--- resource/auth.py
+++ resource/auth.py
@@ -1,6 +1,9 @@
 from flask import g, jsonify
+from flask_restful import marshal
+
 from BaseClass import WaterBase
 from flaskBase import auth
+from resource.user import user_fields
 from sqlalch import User
 
 
@@ -8,4 +11,4 @@ class Auth(WaterBase):
     @auth.login_required
     def get(self):
         token = g.user.generate_auth_token()
-        return jsonify({'token': token.decode('ascii')})
+        return jsonify({'token': token.decode('ascii'), 'user': marshal(g.user, user_fields)})
