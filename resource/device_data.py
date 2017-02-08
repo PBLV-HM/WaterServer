3c410eb2ee693f04c07cf2d89860bdabcc4d659e
Kevin Stieglitz <stieglit@hm.edu>
2017-02-08 09:45:31 +0100

    Get device data

--- resource/device_data.py
+++ resource/device_data.py
@@ -9,20 +9,32 @@ from flask import request
 import json
 from flask_restful import reqparse, marshal, fields, marshal_with, abort
 
+char_data_fields = {
+    'label': fields.String,
+    'value': fields.Float,
+}
+
 device_data_fields = {
-    'time_interval': fields.String,
-    'degree': fields.Float,
-    'dist': fields.Float,
-    'wet': fields.Float
+    'degree': fields.List(fields.Nested(char_data_fields)),
+    'dist': fields.List(fields.Nested(char_data_fields)),
+    'wet': fields.List(fields.Nested(char_data_fields)),
 }
 
 class DeviceData(WaterBase):
     decorators = [auth.login_required]
 
-    def get(self, id, time_from):
-        tfrom = datetime.datetime.fromtimestamp(int(time_from))
-        sql = Data.sql_data_query(5, g.user.id)
+    def get(self, id, interval):
+        sql = Data.sql_data_query(interval, g.user.id)
 
         result = db.engine.execute(sql)
 
-        return [marshal(data, device_data_fields) for data in result]
+        deg_data = [];
+        wet_data = [];
+        dist_data = [];
+        for row in result:
+            deg_data.append({"label": row.time_interval, "value": row.degree})
+            wet_data.append({"label": row.time_interval, "value": row.wet})
+            dist_data.append({"label": row.time_interval, "value": row.dist})
+
+        data = {'degree': deg_data, 'dist': dist_data, 'wet': wet_data}
+        return marshal(data, device_data_fields)
