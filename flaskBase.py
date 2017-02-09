e517dcccfa97dbd5035aa67607d48791c6c82679
Kevin Stieglitz <stieglit@hm.edu>
2017-02-09 09:13:38 +0100

    Add flask cors

--- flaskBase.py
+++ flaskBase.py
@@ -1,12 +1,15 @@
 from flask import Flask, abort, request, jsonify, g, url_for
+from flask_cors import CORS
 from flask_httpauth import HTTPBasicAuth
 from flask_sqlalchemy import SQLAlchemy
 import flask_restful as restful
 
 app = Flask(__name__)
+CORS(app)
+
 # initialization
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1/hmpblv'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@127.0.0.1/hmpblv'
 app.config['SQLALCHEMY_ECHO'] = True
 #app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
